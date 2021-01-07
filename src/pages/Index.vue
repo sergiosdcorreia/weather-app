70d02e99b8f0a5f7be48d3c280b450560dcadb36
Sergio Correia <sergiosdcorreia@gmail.com>
2021-01-07 02:05:20 +0000

    Adding background gradient

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,5 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+  <q-page class="flex column">
   </q-page>
 </template>
 
@@ -12,3 +8,8 @@ export default {
   name: 'PageIndex'
 }
 </script>
+
+<style lang="sass">
+  .q-page
+    background: linear-gradient(to bottom, #136a8a, #267871)
+</style>
