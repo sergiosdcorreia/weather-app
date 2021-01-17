d0752c38eda153409051128e7a6d60de31c9a696
Sergio Correia <sergiosdcorreia@gmail.com>
2021-01-17 08:44:59 +0000

    Added feels like temp

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -36,15 +36,19 @@
         <div class="text-h4 text-weight-light">
           {{ weatherData.name }}
         </div>
-        <div class="text-h6 text-weight-light">
+        <div class="text-h6 text-weight-light q-mt-md">
           {{ weatherData.weather[0].main }}
-        </div>
-        <div class="text-h8 text-weight-light q-mt-md">
-          {{ Math.round(weatherData.wind.speed) }} m/s
         </div>
         <div class="text-h1 text-weight-thin q-my-lg relative-position">
           <span>{{ Math.round(weatherData.main.temp) }}</span>
           <span class="text-h4 relative-position degree">&deg;C</span>
+        </div>
+        <div class="text-h8 text-weight-thin q-my-md relative-position">
+          <span>Feels like {{ Math.round(weatherData.main.feels_like) }}</span>
+          <span class="text-h8 relative-position degree-small">&deg;C</span>
+        </div>
+        <div class="text-h8 text-weight-light q-mt-md">
+          wind {{ Math.round(weatherData.wind.speed) }} m/s
         </div>
       </div>
       <div class="col text-center">
@@ -153,6 +157,8 @@ export default {
       background: linear-gradient(to bottom, #00b4db, #00B3b0)
   .degree
     top: -44px
+  .degree-small
+    top: 0px
   .skyline
     flex: 0 0 100px
     background: url(/skyline.png)
